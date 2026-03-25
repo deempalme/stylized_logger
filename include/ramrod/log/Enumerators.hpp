de80455adf1bf80d4d7cc8e5eeab33d5a83241b2
deempalme <20066249+deempalme@users.noreply.github.com>
2026-03-24 17:39:42 -0700

    [level] Correct info and debug levels

--- include/ramrod/log/Enumerators.hpp
+++ include/ramrod/log/Enumerators.hpp
@@ -12,10 +12,10 @@ enum class LogLevel : std::uint8_t
 {
     /// @brief Verbose level
     VERBOSE = 0u,
-    /// @brief Debug level
-    DEBUG = 1u,
     /// @brief Info level
-    INFO = 2u,
+    INFO = 1u,
+    /// @brief Debug level
+    DEBUG = 2u,
     /// @brief Warning level
     WARNING = 3u,
     /// @brief Error level
